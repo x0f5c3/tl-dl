d7c99e688457b74aa16d86657a3039c753830304
x0f5c3 <x0f5c3@tutanota.com>
2022-05-07 22:25:07 +0200

    Change version to v0.0.3

--- cmd/root.go
+++ cmd/root.go
@@ -14,7 +14,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:     "toolbox-download",
 	Short:   "A tool and a library to download the jetbrains-toolbox",
-	Version: "v0.0.1", // <---VERSION---> Updating this version, will also create a new GitHub release.
+	Version: "v0.0.3", // <---VERSION---> Updating this version, will also create a new GitHub release.
 	// Uncomment the following lines if your bare application has an action associated with it:
 	RunE: runFunc,
 	Args: cobra.ExactArgs(1),
